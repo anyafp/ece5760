7f64dd55cc27f553e7a91027a195c4fde20e95ad
Anya Prabowo <67381009+anyafp@users.noreply.github.com>
2023-03-04 16:00:23 -0500

    print vals and change iter value

--- lab2w2.c
+++ lab2w2.c
@@ -15,10 +15,11 @@
 #include <sys/mman.h>
 #include <sys/time.h> 
 #include <math.h> 
-
 #include "address_map_arm.h" 
 
-/* function prototypes */
+// =============================================
+// Function Prototypes
+// =============================================
 void VGA_text (int, int, char *);
 void VGA_text_clear();
 void VGA_box (int, int, int, int, short);
@@ -46,62 +47,126 @@ void *vga_char_virtual_base;
 // /dev/mem file id
 int fd;
 
-volatile signed int* x_pio_ptr = NULL;
-volatile signed int* y_pio_ptr = NULL;
-volatile signed int* incr_x_pio_ptr = NULL; 
-volatile signed int* incr_y_pio_ptr = NULL;
-volatile unsigned char* reset_pio_ptr = NULL;
-
-#define X_PIO                 0x00
-#define Y_PIO                 0x10
-#define X_INCR_PIO            0x20
-#define Y_INCR_PIO				    0x30
-#define RST_PIO				        0x40
-
-// pixel macro
-// !!!PACKED VGA MEMORY!!!
+// =============================================
+// pixel macro !!!PACKED VGA MEMORY!!!
+// =============================================
 #define VGA_PIXEL(x,y,color) do{\
 	char  *pixel_ptr ;\
 	pixel_ptr = (char *)vga_pixel_ptr + ((y)*640) + (x) ;\
 	*(char *)pixel_ptr = (color);\
 } while(0)
-	
 
-// measure time
-struct timeval t1, t2;
-double elapsedTime;
-struct timespec delay_time ;
+// =============================================
+// PIO Pointers
+// =============================================
+#define X_PIO                 0x00
+#define Y_PIO                 0x10
+#define X_INCR_PIO            0x20
+#define Y_INCR_PIO				    0x30
+#define RST_PIO				        0x40
+#define ITER_PIO				      0x50
 
+volatile signed int* x_pio_ptr = NULL;
+volatile signed int* y_pio_ptr = NULL;
+volatile signed int* incr_x_pio_ptr = NULL; 
+volatile signed int* incr_y_pio_ptr = NULL;
+volatile unsigned char* reset_pio_ptr = NULL;
+volatile unsigned int* iter_pio_ptr = NULL;
+
+// =============================================
+// Fixed point functions
+// =============================================
 int float2fix(float);
-
 int float2fix(float a) { return (int)(a*8388608); }
 
+// =============================================
+// Measure time
+// =============================================
+struct timeval t1, t2;
+double elapsedTime;
+struct timespec delay_time;
+char xy_topleft[10], xy_topright[10], xy_bottomleft[10], xy_bottomright[10];
+char zoom_print[10], mouse_coord[20]; 
+char num_string[20], time_string[50];
+char new_coord[40], zoom_val[20], iter_text[40];
+
+// =============================================
+// Variables
+// =============================================
+float x_incr = 0.0046875;        // Real increment value
+float y_incr = 0.00416666666667; // Imaginary increment value
+float x_val = -2.0;              // Top left real coordinates
+float y_val = 1.0;               // Top left imaginary coordinates
+int x_vga = 0;
+int y_vga = 0;
+int x_accum = 0;
+int y_accum = 0;
+float zoom_x = 1;
+
+// =============================================
+// Print data function
+// =============================================
+void print_data(float x_, float y_, float x_incr, float y_incr, float zoom, int iter) {
+  VGA_text_clear();
+  
+  // Top left xy
+  sprintf( xy_topleft, "(%4.4f, %4.4f)", x_, y_ );
+	VGA_text (1, 1, xy_topleft);
+ 
+  // Top right xy
+  sprintf( xy_bottomleft, "(%4.4f, %4.4f)", (x_+(x_incr*640)), y_ );
+	VGA_text (58, 1, xy_bottomleft);
+  
+  // Bottom right xy
+  sprintf( xy_topright, "(%4.4f, %4.4f)", (x_+(x_incr*640)), (y_+(y_incr*480)) );
+	VGA_text (58, 58, xy_topright);
+ 
+  // Bottom left xy
+  sprintf( xy_bottomleft, "(%4.4f, %4.4f)", x_, (y_+(y_incr*480)) );
+	VGA_text (1, 58, xy_bottomleft);
+ 
+  // middle
+  sprintf( new_coord, "Middle xy: (%4.4f, %4.4f)", (x_+(x_incr*320)), (y_+(y_incr*240)) );
+  VGA_text (10, 6, new_coord);
+  
+  // zoom
+  sprintf( zoom_val, "Zoom: %2.3f x", zoom );
+  VGA_text (10, 8, zoom_val);
+  
+  // iter
+  sprintf( iter_text, "Max Iter: %d", iter );
+  VGA_text (10, 10, iter_text);
+  
+  VGA_text (10, 4, time_string);
+ 	
+}
+
+
+// =============================================
+// Main
+// =============================================
 int main(void)
 {
-  // === MOUSE =======================
-  
+  // ---------------------------------------------
+  // Mouse setup
+  // ---------------------------------------------
   int fdm, bytes;
   unsigned char data[3];
-
   const char *pDevice = "/dev/input/mice";
-
-  // Open Mouse
-  fdm = open(pDevice, O_RDWR);
-  if(fdm == -1)
-  {
+  fdm = open(pDevice, O_RDWR); // Open Mouse
+  if(fdm == -1) {
       printf("ERROR Opening %s\n", pDevice);
       return -1;
   }
-
-  int left, middle, right;
-  signed char x, y;
-  // ==========================
+  int left, middle, right; // Button presses
+  signed char x, y;        // Mouse coordinates
   
 	delay_time.tv_nsec = 10 ;
 	delay_time.tv_sec = 0 ;
 
-	// Declare volatile pointers to I/O registers (volatile 	// means that IO load and store instructions will be used 	// to access these pointer locations, 
-	// instead of regular memory loads and stores) 
+	// ---------------------------------------------
+  // Get addresses to map
+  // ---------------------------------------------
   	
 	// === need to mmap: =======================
 	// FPGA_CHAR_BASE
@@ -109,13 +174,13 @@ int main(void)
 	// HW_REGS_BASE        
   
 	// === get FPGA addresses ==================
-    // Open /dev/mem
+  // Open /dev/mem
 	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) 	{
 		printf( "ERROR: could not open \"/dev/mem\"...\n" );
 		return( 1 );
 	}
     
-    // get virtual addr that maps to physical
+  // get virtual addr that maps to physical
 	// for light weight bus
 	h2p_lw_virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );	
 	if( h2p_lw_virtual_base == MAP_FAILED ) {
@@ -124,35 +189,35 @@ int main(void)
 		return(1);
 	}
  
+  // === get PIO pointer addresses ==================
   x_pio_ptr = (signed int *)(h2p_lw_virtual_base + X_PIO);
 	y_pio_ptr = (signed int *)(h2p_lw_virtual_base + Y_PIO);
   incr_x_pio_ptr = (signed int *)(h2p_lw_virtual_base + X_INCR_PIO);
 	incr_y_pio_ptr = (signed int *)(h2p_lw_virtual_base + Y_INCR_PIO);
 	reset_pio_ptr = (unsigned char *)(h2p_lw_virtual_base + RST_PIO);
-	
+	iter_pio_ptr = (unsigned int *)(h2p_lw_virtual_base + ITER_PIO);
+ 
 	// === get VGA char addr =====================
 	// get virtual addr that maps to physical
-	vga_char_virtual_base = mmap( NULL, FPGA_CHAR_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, FPGA_CHAR_BASE );	
+	vga_char_virtual_base = mmap( NULL, FPGA_CHAR_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, FPGA_CHAR_BASE  );	
 	if( vga_char_virtual_base == MAP_FAILED ) {
 		printf( "ERROR: mmap2() failed...\n" );
 		close( fd );
 		return(1);
 	}
     
-    // Get the address that maps to the character 
+  // Get the address that maps to the character 
 	vga_char_ptr =(unsigned int *)(vga_char_virtual_base);
 
 	// === get VGA pixel addr ====================
-	// get virtual addr that maps to physical
-	// SDRAM
+	// get virtual addr that maps to physical SDRAM
 	vga_pixel_virtual_base = mmap( NULL, FPGA_ONCHIP_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, SDRAM_BASE); //SDRAM_BASE	
-	
 	if( vga_pixel_virtual_base == MAP_FAILED ) {
 		printf( "ERROR: mmap3() failed...\n" );
 		close( fd );
 		return(1);
 	}
-    // Get the address that maps to the FPGA pixel buffer
+  // Get the address that maps to the FPGA pixel buffer
 	vga_pixel_ptr =(unsigned int *)(vga_pixel_virtual_base);
 	
 	// === get RAM FPGA parameter addr =========
@@ -163,149 +228,157 @@ int main(void)
 		close( fd );
 		return(1);
 	}
-    // Get the address that maps to the RAM buffer
-	sram_ptr =(unsigned int *)(sram_virtual_base);
+  // Get the address that maps to the RAM buffer
+	sram_ptr = (unsigned int *)(sram_virtual_base);
  
-	// ===========================================
+	// ---------------------------------------------
+  // Text on VGA
+  // ---------------------------------------------
 
-	/* create a message to be displayed on the VGA 
-          and LCD displays */
-	char text_top_row[40] = "DE1-SoC ARM/FPGA\0";
-	char text_bottom_row[40] = "Cornell ece5760\0";
-	char num_string[20], time_string[50] ;
-	
-	// a pixel from the video
-	int pixel_color;
-	// video input index
-	int i,j, count;
+	int pixel_color; // a pixel from the video
+	int i,j, count; // video input index
 	
 	// clear the screen
-//	VGA_box (0, 0, 639, 479, 0x03);
+  // VGA_box (0, 0, 639, 479, 0x03);
 	// clear the text
 	VGA_text_clear();
-	VGA_text (1, 56, text_top_row);
-	VGA_text (1, 57, text_bottom_row);
  
- *(sram_ptr)=0;
+  *(sram_ptr) = 0; // Reset timing
 	
 	count = 0;
- 
-  float x_incr = 0.0046875;
-  float y_incr = 0.00416666666667;
-  float x_val = -2.0;
-  float y_val = 1.0;
+  
+  // ---------------------------------------------
+  // Variables for mouse
+  // ---------------------------------------------
+  float x_incr = 0.0046875;        // Real increment value
+  float y_incr = 0.00416666666667; // Imaginary increment value
+  float x_val = -2.0;              // Top left real coordinates
+  float y_val = 1.0;               // Top left imaginary coordinates
   int x_vga = 0;
   int y_vga = 0;
-  
   int x_accum = 0;
   int y_accum = 0;
   
+  // ---------------------------------------------
+  // Initialize real and im starting points and incr values
+  // ---------------------------------------------
   *(x_pio_ptr) = float2fix(x_val);
   *(y_pio_ptr) = float2fix(y_val);
   *(incr_x_pio_ptr) = float2fix(x_incr);
   *(incr_y_pio_ptr) = float2fix(y_incr);
   
-  // reset to init values
+  // ---------------------------------------------
+  // Reset sent to FPGA
+  // ---------------------------------------------
 	*(reset_pio_ptr) = 0;
   usleep( 1 );
 	*(reset_pio_ptr) = 1;
   
-	while(1) 
-	{
-    printf("before start drawing\n");
+  // =============================================
+  // While 1 loop
+  // =============================================
+	while(1) {
+ 
     // after first reset, fpga in state 3
     while (*(sram_ptr)==0) {
+    
       // === MOUSE =======================    
       bytes = read(fdm, data, sizeof(data));
   
-      if(bytes > 0)
-      {
-          left = data[0] & 0x1;
-          right = data[0] & 0x2;
-          middle = data[0] & 0x4;
-  
-          x = data[1];
-          y = data[2];
-          printf("x=%d, y=%d, left=%d, middle=%d, right=%d\n", x, y, left, middle, right);
-          
-          x_accum += x/2;
-          y_accum += y/2;
-          
-          printf( "current coord = %d, %d, accum_x = %d, accum_y = %d\n", x_vga, y_vga, x_accum, y_accum);
-          
-          // zoom in if not maxed out
-          if ( left == 1 && x_incr > 0.00000000715255 && y_incr > 0.00000000635788 ) {
-            x_incr = x_incr/2;
-            y_incr = y_incr/2;
-            *(incr_x_pio_ptr) = float2fix(x_incr);
-            *(incr_y_pio_ptr) = float2fix(y_incr);
-            *(reset_pio_ptr) = 0;
-            usleep( 1 );
-          	*(reset_pio_ptr) = 1;
-            x_accum = 0;
-            y_accum = 0;
-          // zoom out
-          } else if ( right == 2 && x < 4.9 && y < 4.3 ) {
-            x_incr = x_incr*2;
-            y_incr = y_incr*2;
-            *(incr_x_pio_ptr) = float2fix(x_incr);
-            *(incr_y_pio_ptr) = float2fix(y_incr);
-            *(reset_pio_ptr) = 0;
-            usleep( 1 );
-          	*(reset_pio_ptr) = 1;
-            x_accum = 0;
-            y_accum = 0;
-          } else if ( middle == 4 ) {
-            if ( x_val + x_accum*x_incr < 640 || x_val + x_accum*x_incr > 0 ) {
-              x_val = x_val + x_accum*x_incr;
-              x_vga = x_vga + x_accum;
-            }
-            if ( y_val + y_accum*y_incr < 480 || y_val + y_accum*y_incr > 0 ) {
-              y_val = y_val + y_accum*y_incr;
-              y_vga = y_vga + y_accum;
-            }
-            *(x_pio_ptr) = float2fix(x_val);
-            *(y_pio_ptr) = float2fix(y_val);
-            *(reset_pio_ptr) = 0;
-            usleep( 1 );
-          	*(reset_pio_ptr) = 1;
-            x_accum = 0;
-            y_accum = 0;
-          }
-            
-      } // if bytes > 0
+      if (bytes > 0) {
+      
+        left = data[0] & 0x1;
+        right = data[0] & 0x2;
+        middle = data[0] & 0x4;
     
+        x = data[1];
+        y = data[2];
+        
+        x_accum += x/2;
+        y_accum += y/2;
+        
+        // middle
+        float x_temp = x_val + x_accum*x_incr + x_incr*320;
+        float y_temp = y_val + y_accum*y_incr + y_incr*240;
+        
+        sprintf( new_coord, "Middle xy: (%4.4f, %4.4f)", x_temp, y_temp );
+        VGA_text (10, 6, new_coord);
+        
+        // zoom in if not maxed out
+        if ( left == 1 && x_incr > 0.000000715255 && y_incr > 0.000000635788 ) {
+        
+          x_incr = x_incr/2;
+          y_incr = y_incr/2;
+          *(incr_x_pio_ptr) = float2fix(x_incr);
+          *(incr_y_pio_ptr) = float2fix(y_incr);
+          *(reset_pio_ptr) = 0;
+          usleep( 1 );
+        	*(reset_pio_ptr) = 1;
+          zoom_x = zoom_x*2;
+    
+          print_data(x_val, y_val, x_incr, y_incr, zoom_x, *(iter_pio_ptr)); // Print new info
+          
+          // Reset accum
+          x_accum = 0;
+          y_accum = 0;
+          
+        // zoom out
+        } else if ( right == 2 && x < 4.9 && y < 4.3 ) {
+          x_incr = x_incr*2;
+          y_incr = y_incr*2;
+          *(incr_x_pio_ptr) = float2fix(x_incr);
+          *(incr_y_pio_ptr) = float2fix(y_incr);
+          *(reset_pio_ptr) = 0;
+          usleep( 1 );
+        	*(reset_pio_ptr) = 1;
+          zoom_x = zoom_x/2;
+         
+          print_data(x_val, y_val, x_incr, y_incr, zoom_x, *(iter_pio_ptr)); // Print new info
+          
+          // Reset accum
+          x_accum = 0;
+          y_accum = 0;
+        
+        // Reset to new coordinates  
+        } else if ( middle == 4 ) {
+        
+          x_val = x_val + x_accum*x_incr;
+          y_val = y_val + y_accum*y_incr;
+            
+          *(x_pio_ptr) = float2fix(x_val);
+          *(y_pio_ptr) = float2fix(y_val);
+          *(reset_pio_ptr) = 0;
+          usleep( 1 );
+        	*(reset_pio_ptr) = 1;
+         
+          print_data(x_val, y_val, x_incr, y_incr, zoom_x, *(iter_pio_ptr)); // Print new info
+          
+          // Reset accum
+          x_accum = 0;
+          y_accum = 0;
+        }    
+      } // if bytes > 0
+      
     } // while sram ptr = 0
 
-		gettimeofday(&t1, NULL);
-   
-    printf("start drawing\n");
+		gettimeofday(&t1, NULL); // Start timing
 	
 		// wait for FPGA to zero the "data_ready" flag
-		while (*(sram_ptr)==1) ;
-   
-    printf("done drawing\n");
-		
+		while (*(sram_ptr)==1) ; // FPGA is done drawing
+    
 		// === time the fpga =======================
 		gettimeofday(&t2, NULL);
 		elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000.0;      // sec to us
 		elapsedTime += (t2.tv_usec - t1.tv_usec) ;   // us to 
 		//sprintf(num_string, "# = %d     ", total_count);
-		sprintf(time_string, "T=%8.0f uSec  ", elapsedTime);
+		sprintf(time_string, "T =%8.0f uSec  ", elapsedTime);
 		//VGA_text (10, 3, num_string);
-		VGA_text (10, 4, time_string);
+		//VGA_text (10, 4, time_string);
+    
+    print_data(x_val, y_val, x_incr, y_incr, zoom_x, *(iter_pio_ptr)); // Print new info
 
 	} // end while(1)
 } // end main
-
-/****************************************************************************************
- * Subroutine to read a pixel from the video input 
-****************************************************************************************/
-// int  video_in_read_pixel(int x, int y){
-	// char  *pixel_ptr ;
-	// pixel_ptr = (char *)video_in_ptr + ((y)<<9) + (x) ;
-	// return *pixel_ptr ;
-// }
 
 /****************************************************************************************
  * Subroutine to read a pixel from the VGA monitor 
